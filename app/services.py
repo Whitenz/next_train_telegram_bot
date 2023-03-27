72310ce45edd0ceeb59c73446f6dd6d7832d10e4
Ilya Kolesnikov <zloy-gamer@yandex.ru>
2023-03-27 23:21:56 +0500

    Replaced namedtuple with dataclass for schedule obj and add time formatter

--- app/services.py
+++ app/services.py
@@ -1,27 +1,31 @@
 import datetime as dt
-from collections import namedtuple
-
-from config import (CLOSE_TIME_METRO, DB_FILENAME, LIMIT_ROW,
-                    OPEN_TIME_METRO, SQL_QUERY, MAX_AWAIT_TRAIN)
+from dataclasses import dataclass
 
 import aiosqlite
 
+from config import (CLOSE_TIME_METRO, DB_FILENAME, LIMIT_ROW,
+                    OPEN_TIME_METRO, TIME_TO_TRAIN_QUERY)
 
-class Schedule(namedtuple('Train', ['from_station',
-                                    'to_station',
-                                    'time_to_train'])):
-    """Кастомный namedtuple для удобной обработки записей из БД."""
-    __slots__ = ()
+
+@dataclass
+class Schedule:
+    from_station: str
+    to_station: str
+    time_to_train: str
+
+    def __post_init__(self):
+        datetime_obj = dt.datetime.strptime(self.time_to_train, '%H:%M:%S')
+        self.time_to_train = datetime_obj.strftime('%M:%S')
 
     @property
     def direction(self):
         return f'{self.from_station} ➡ {self.to_station}'
 
 
-def namedtuple_factory(cursor, row):
+def custom_rowfactory(cursor, row):
     """
-    row_factory, который будет возвращать записи из БД в виде заданного
-    именованного кортежа.
+    row_factory, который будет возвращать записи из БД в виде экземпляров
+     дата-класса Scheduleю
     """
     return Schedule(*row)
 
@@ -42,24 +46,23 @@ async def get_schedule(from_station: str,
                        to_station: str) -> list[Schedule, ...]:
     """Функция делает запрос к БД с переданными аргументами.
     Возвращает список с объектами именованного кортежа."""
-    parameters = (from_station, to_station, is_weekend(),
-                  MAX_AWAIT_TRAIN, LIMIT_ROW)
+    parameters = (from_station, to_station, is_weekend(), LIMIT_ROW)
     async with aiosqlite.connect(DB_FILENAME) as db:
-        db.row_factory = namedtuple_factory
-        async with db.execute(SQL_QUERY, parameters) as cursor:
+        db.row_factory = custom_rowfactory
+        async with db.execute(TIME_TO_TRAIN_QUERY, parameters) as cursor:
             return await cursor.fetchall()
 
 
 def get_text_with_time_to_train(schedule):
-    if len(schedule) == 2:
+    if len(schedule) >= 2:
         return (
             f'<b>{schedule[0].direction}:</b>\n\n'
-            f'ближайший поезд через {schedule[0].time_to_train} (ч:мин:с)\n'
-            f'следующий через {schedule[1].time_to_train} (ч:мин:с)'
+            f'ближайший поезд через {schedule[0].time_to_train} (мин:с)\n'
+            f'следующий через {schedule[1].time_to_train} (мин:с)'
         )
     elif len(schedule) == 1:
         return (
             f'<b>{schedule[0].direction}:</b>\n\n'
-            f'последний поезд через {schedule[0].time_to_train} (ч:мин:с)'
+            f'последний поезд через {schedule[0].time_to_train} (мин:с)'
         )
     return f'По расписанию поездов сегодня больше нет.'

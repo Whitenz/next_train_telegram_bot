531c7e3afc26d54e4005950b5bf21d08c3ba796a
Ilya Kolesnikov <zloy-gamer@yandex.ru>
2023-04-10 22:20:11 +0500

    Add command /stations for older users in schedule_handler

--- app/config.py
+++ app/config.py
@@ -111,7 +111,7 @@ CLOSE_TIME_METRO = dt.time(hour=0, minute=30)
 # Команды, которые обрабатывает бот
 START_COMMAND = 'start'
 HELP_COMMAND = 'help'
-SCHEDULE_COMMAND = 'schedule'
+SCHEDULE_COMMAND = ('schedule', 'stations')  # stations - устаревшая команда
 FAVORITES_COMMAND = 'favorites'
 ADD_FAVORITE_COMMAND = 'add_favorite'
 CLEAR_FAVORITES_COMMAND = 'clear_favorites'

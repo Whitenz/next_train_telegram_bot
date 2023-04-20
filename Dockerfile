4311666a2cbd16123a069f53f43f5703f68e6d7b
Ilya Kolesnikov <zloy-gamer@yandex.ru>
2023-04-20 22:43:39 +0500

    refactor: combine two conv for "schedule" and "add_favorite" commands to single

--- Dockerfile
+++ Dockerfile
@@ -4,7 +4,7 @@ ENV TZ="Asia/Yekaterinburg"
 
 WORKDIR /src
 
-COPY . .
+COPY src .
 
 RUN pip3 install --no-cache-dir -r requirements.txt
 
--- src/app/config.py
+++ src/app/config.py
@@ -3,7 +3,6 @@ import os
 
 from dotenv import load_dotenv
 
-# Получаем токен бота
 load_dotenv()
 BOT_TOKEN = os.getenv('BOT_TOKEN')
 
@@ -13,8 +12,7 @@ DB_FILENAME = os.path.join(BASE_DIR, 'data', 'schedule.sqlite3')
 LIMIT_ROW = 2  # берем из БД для бота только два ближайших поезда
 
 # Параметры для ConversationHandler's
-CHOICE_DIRECTION, GET_TIME_TO_TRAIN = range(2)
-NEW_FAVORITE = 1
+CHOICE_DIRECTION, FINAL_CONV = range(2)
 CONVERSATION_TIMEOUT = 60 * 3  # время ожидания ответа от пользователя (сек)
 
 # Часы работы метрополитена. Интервал расширен на 0.5 часа в обе стороны

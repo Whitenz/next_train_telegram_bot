c3b5eeecd27870299eced0ea4987eb2c466f9913
Ilya Kolesnikov <zloy-gamer@yandex.ru>
2023-05-08 18:01:53 +0500

    refactor: add "init" attr for not required column, change syntax contraints

--- src/app/models.py
+++ src/app/models.py
@@ -1,12 +1,10 @@
 import datetime
 from typing import List
 
-from sqlalchemy import (CheckConstraint, ForeignKey, func, String,
-                        UniqueConstraint)
-from sqlalchemy.dialects.postgresql import TIME
-from sqlalchemy.orm import (column_property, DeclarativeBase, Mapped,
-                            mapped_column,
-                            MappedAsDataclass, relationship)
+from sqlalchemy import (TIME, CheckConstraint, ForeignKey, String,
+                        UniqueConstraint, func)
+from sqlalchemy.orm import (DeclarativeBase, Mapped, MappedAsDataclass,
+                            column_property, mapped_column, relationship)
 from typing_extensions import Annotated
 
 TIMESTAMP_TYPE = Annotated[
@@ -29,61 +27,74 @@ class Base(MappedAsDataclass, DeclarativeBase):
 class Station(Base):
     __tablename__ = 'station'
 
-    station_id: Mapped[int] = mapped_column(primary_key=True)
+    station_id: Mapped[int] = mapped_column(init=False, primary_key=True)
     station_name: Mapped[str] = mapped_column(String(30), unique=True)
 
 
 class Schedule(Base):
     __tablename__ = 'schedule'
 
-    schedule_id: Mapped[int] = mapped_column(primary_key=True)
-    from_station_id: Mapped[STATION_FK] = mapped_column('from_station_id')
-    to_station_id: Mapped[STATION_FK] = mapped_column('to_station_id')
-    is_weekend: Mapped[bool]
-    departure_time: Mapped[datetime.time] = mapped_column('departure_time')
+    schedule_id: Mapped[int] = mapped_column(init=False, primary_key=True)
+    from_station_id: Mapped[STATION_FK] = mapped_column()
+    to_station_id: Mapped[STATION_FK] = mapped_column()
+    is_weekend: Mapped[bool] = mapped_column()
+    departure_time: Mapped[datetime.time] = mapped_column()
 
     time_to_train: Mapped[datetime.time] = column_property(
-        func.cast(departure_time - func.localtime(), TIME),
+        column=func.cast(departure_time - func.localtime(), TIME), init=False
     )
-    from_station_obj: Mapped['Station'] = relationship(foreign_keys=from_station_id)
-    to_station_obj: Mapped['Station'] = relationship(foreign_keys=to_station_id)
+    from_station_obj: Mapped['Station'] = relationship(
+        init=False, foreign_keys=from_station_id
+    )
+    to_station_obj: Mapped['Station'] = relationship(
+        init=False, foreign_keys=to_station_id
+    )
 
     __table_args__ = (
-        CheckConstraint('from_station_id != to_station_id', name='route_check'),
-        UniqueConstraint('from_station_id', 'to_station_id', 'is_weekend',
-                         'departure_time', name='schedule_unique')
+        CheckConstraint(from_station_id != to_station_id, name='route_check'),
+        UniqueConstraint(from_station_id, to_station_id, is_weekend,
+                         departure_time, name='schedule_unique')
     )
 
 
 class BotUser(Base):
     __tablename__ = 'bot_user'
 
-    bot_user_id: Mapped[int] = mapped_column(primary_key=True, autoincrement=False)
+    bot_user_id: Mapped[int] = mapped_column(
+        primary_key=True, autoincrement=False, unique=True
+    )
     first_name: Mapped[str]
     last_name: Mapped[str | None]
     username: Mapped[str | None] = mapped_column(unique=True)
     is_bot: Mapped[bool]
-    created_at: Mapped[TIMESTAMP_TYPE]
+    created_at: Mapped[TIMESTAMP_TYPE] = mapped_column(init=False)
 
     favorites: Mapped[List['Favorite']] = relationship(
-        back_populates='bot_user', cascade='all, delete-orphan')
+        init=False, back_populates='bot_user', cascade='all, delete-orphan'
+    )
 
 
 class Favorite(Base):
     __tablename__ = 'favorite'
 
-    favorite_id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
-    bot_user_id: Mapped[int] = mapped_column(
-        ForeignKey('bot_user.bot_user_id', ondelete='CASCADE', onupdate='CASCADE')
+    favorite_id: Mapped[int] = mapped_column(
+        init=False, primary_key=True, autoincrement=True
     )
-    from_station_id: Mapped[STATION_FK] = mapped_column('from_station_id')
-    to_station_id: Mapped[STATION_FK] = mapped_column('to_station_id')
+    bot_user_id: Mapped[int] = mapped_column(
+        ForeignKey('bot_user.bot_user_id', ondelete='CASCADE', onupdate='CASCADE'),
+    )
+    from_station_id: Mapped[STATION_FK] = mapped_column()
+    to_station_id: Mapped[STATION_FK] = mapped_column()
 
-    from_station_obj: Mapped['Station'] = relationship(foreign_keys=from_station_id)
-    to_station_obj: Mapped['Station'] = relationship(foreign_keys=to_station_id)
-    bot_user: Mapped['BotUser'] = relationship(back_populates='favorites')
+    from_station_obj: Mapped['Station'] = relationship(
+        init=False, foreign_keys=from_station_id
+    )
+    to_station_obj: Mapped['Station'] = relationship(
+        init=False, foreign_keys=to_station_id
+    )
+    bot_user: Mapped['BotUser'] = relationship(init=False, back_populates='favorites')
 
     __table_args__ = (
-        UniqueConstraint('bot_user_id', 'from_station_id', 'to_station_id',
+        UniqueConstraint(bot_user_id, from_station_id, to_station_id,
                          name='favorite_unique'),
     )
